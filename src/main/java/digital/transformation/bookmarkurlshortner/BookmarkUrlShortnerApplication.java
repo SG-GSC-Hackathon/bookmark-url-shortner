991241748c013bffee14c205478e769f5d00aee1
Kavana <kavana.tad@gmail.com>
2020-09-07 21:43:21 +0530

    Initial commit

---
+++ src/main/java/digital/transformation/bookmarkurlshortner/BookmarkUrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package digital.transformation.bookmarkurlshortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkUrlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkUrlShortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digital/transformation/bookmarkurlshortner/BookmarkUrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.transformation.bookmarkurlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkUrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
